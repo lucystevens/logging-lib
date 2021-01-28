6d685afeec465c54fc21cf416eea6c97fdcd5e56
Luke Stevens <lcm.stevens96@gmail.com>
2021-01-28 22:06:41 +0000

    Specify LoggingLevel in provider

--- src/main/java/uk/co/lukestevens/logging/provider/ConsoleLoggingProvider.java
+++ src/main/java/uk/co/lukestevens/logging/provider/ConsoleLoggingProvider.java
@@ -1,5 +1,7 @@
 package uk.co.lukestevens.logging.provider;
 
+import javax.inject.Inject;
+
 import uk.co.lukestevens.logging.Logger;
 import uk.co.lukestevens.logging.LoggerLevel;
 import uk.co.lukestevens.logging.LoggingProvider;
@@ -7,9 +9,16 @@ import uk.co.lukestevens.logging.loggers.ConsoleLogger;
 
 public class ConsoleLoggingProvider implements LoggingProvider {
 	
+	final LoggerLevel loggerLevel;
+	
+	@Inject
+	public ConsoleLoggingProvider(LoggerLevel loggerLevel) {
+		this.loggerLevel = loggerLevel;
+	}
+
 	@Override
 	public Logger getLogger(String name) {
-		return new ConsoleLogger(name, LoggerLevel.INFO);
+		return new ConsoleLogger(name, loggerLevel);
 	}
 
 }
--- src/main/java/uk/co/lukestevens/logging/provider/DatabaseLoggingProvider.java
+++ src/main/java/uk/co/lukestevens/logging/provider/DatabaseLoggingProvider.java
@@ -13,16 +13,18 @@ public class DatabaseLoggingProvider implements LoggingProvider {
 	
 	final Database db;
 	final ApplicationProperties appProperties;
+	final LoggerLevel loggerLevel;
 	
 	@Inject
-	public DatabaseLoggingProvider(Database db, ApplicationProperties appProperties) {
+	public DatabaseLoggingProvider(Database db, ApplicationProperties appProperties, LoggerLevel loggerLevel) {
 		this.db = db;
 		this.appProperties = appProperties;
+		this.loggerLevel = loggerLevel;
 	}
 
 	@Override
 	public Logger getLogger(String name) {
-		return new DatabaseLogger(db, appProperties.getApplicationName(), appProperties.getApplicationVersion(), name, LoggerLevel.INFO);
+		return new DatabaseLogger(db, appProperties.getApplicationName(), appProperties.getApplicationVersion(), name, loggerLevel);
 	}
 
 }
